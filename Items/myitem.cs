03aeeb083746f6588b1e7c18929a941dd25d04b5
Stefano <30950154+Stefifox@users.noreply.github.com>
2021-02-16 11:59:00 +0100

    Add first code

---
+++ Items/myitem.cs
@@ -0,0 +1,29 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TestMod.Items
+{
+    public class myitem : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Woow Esisto?");
+            Tooltip.SetDefault("Sono un item inutile");
+        }
+
+        public override void SetDefaults()
+        {
+            item.damage = 0;
+            item.melee = true;
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.DirtBlock, 10);
+            //recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+
+    }
+}
---
+++ TestMod.cs
@@ -0,0 +1,15 @@
+using Terraria.ModLoader;
+
+namespace TestMod
+{
+	public class TestMod : Mod
+	{
+		public TestMod()
+        {
+			Properties = new ModProperties()
+			{
+				Autoload = true
+			};
+        }
+	}
+}
